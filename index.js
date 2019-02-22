bede2704252d920097de0e1c8ad8795bac45bd1e
Ben N <bentinata@gmail.com>
2019-02-22 15:28:48 +0700

    Render the vue manually.

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const vue = new Vue({
   el: 'main',
   data: {
-    colors: ['#282a29', '#ec1d25', '#4b6688', '#f1f1f4'],
+    colors: ['#282a29ff', '#ec1d25ff', '#4b6688ff', '#f1f1f4ff'],
+  },
+  render(createElement) {
+    return createElement('main', [
+      createElement('form', [
+        createElement('input', {
+          attrs: {
+            id: 'file',
+            name: 'file',
+            type: 'file',
+            accept: 'image/jpeg,image/png',
+          },
+          on: { change: this.upload },
+        }),
+        createElement('label', { attrs: {for: 'file'}}, [
+          createElement('strong', 'Choose a file'),
+          createElement('span', { staticClass: 'dragndrop' }, ' or drag it here'),
+          '.',
+        ]),
+      ]),
+      createElement('li', this.colors.map(color => createElement('ul', {
+        style: { background: color },
+      }, color))),
+    ]);
   },
   methods: {
     async upload(event) {
